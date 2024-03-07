2b3e53a4f1f0530c57a9c49967f7b0f40690868d
Tquandoo <quantrungdo2401@gmail.com>
2024-03-07 23:44:53 +0700

    field email pass

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify'
+import { toast } from 'react-toastify';
 import { useNavigate } from "react-router-dom";
 import './Css/LoginSignup.css';
 import { useDispatch } from "react-redux";
 import { login } from "../slices/authSlice";
 import { API_ROOT } from "../utils/constants";
-// import BeatLoader from "react-spinners/BeatLoader";
-
+import Spinner from "../components/Spinner/Spinner";
 const schemaLogin = yup.object({
   username: yup.string(),
   email: yup.string().required("Email is required"),
@@ -23,29 +22,42 @@ const schemaSignup = yup.object({
 });
 
 const LoginSignup = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [state, setState] = useState("Login");
-  // const [dataForm, setDataForm] = useState('')
+  const [defaultEmail, setDefaultEmail] = useState('tquando24@gmail.com');
+  const [defaultPassword, setDefaultPassword] = useState('2401');
+  const [loading, setLoading] = useState(false);
+
   const { register, handleSubmit, reset, formState: { errors } } = useForm({
-    resolver: yupResolver(state === "Login" ? schemaLogin : schemaSignup)
+    resolver: yupResolver(state === "Login" ? schemaLogin : schemaSignup),
+    defaultValues: {
+      email: defaultEmail,
+      password: defaultPassword
+    }
   });
 
-  const navigate = useNavigate()
-  // useEffect(() => {
-  //   console.log("state:", state)
-  // })
+  const navigate = useNavigate();
 
   const setLogin = () => {
-    setState("Login")
-    reset()
-  }
+    setState("Login");
+    reset();
+  };
+
   const setSignup = () => {
-    setState("Signup")
-    reset()
-  }
+    setState("Signup");
+    reset();
+  };
+
+  useEffect(() => {
+    reset({
+      email: state === "Login" ? defaultEmail : '',
+      password: state === "Login" ? defaultPassword : ''
+    });
+  }, [state]);
 
   const handleSignupSubmit = async (data) => {
-    reset() // Đặt lại form trước khi kiểm tra state
+    setLoading(true)
+    reset();
     let responseData;
     await fetch(`${API_ROOT}/signup`, {
       method: 'POST',
@@ -54,23 +66,26 @@ const LoginSignup = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
-    }).then((res) => (
-      res.json()
-    )).then((data) => {
-      responseData = data
-    })
+    }).then((res) => res.json())
+      .then((data) => {
+        responseData = data;
+      })
+      .finally(() => setLoading(false))
     if (responseData.success) {
-      localStorage.setItem('auth-token', responseData.token)
-      dispatch(login(responseData))
-      toast.success('User registration successful')
-      navigate('/')
+      localStorage.setItem('auth-token', responseData.token);
+      toast.success('User registration successful', {
+        autoClose: 300,
+        onClose: () => navigate('/')
+      });
+      dispatch(login(responseData));
     } else {
-      toast.error(responseData.errors)
+      toast.error(responseData.errors);
     }
-  }
+  };
 
   const handleLoginSubmit = async (data) => {
-    const buildData = { ...data, username: '' }
+    setLoading(true)
+    const buildData = { ...data, username: '' };
     let responseData;
     await fetch(`${API_ROOT}/login`, {
       method: 'POST',
@@ -79,22 +94,22 @@ const LoginSignup = () => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(buildData)
-    }).then((res) => (
-      res.json()
-    )).then((data) => {
-      responseData = data
-    })
-    if (responseData.success) {
-      localStorage.setItem('auth-token', responseData.token)
-      toast.success('Login success', {
-        autoClose: 500,
-        onClose: () => navigate('/')
+    }).then((res) => res.json())
+      .then((data) => {
+        responseData = data;
       })
-      dispatch(login(responseData))
+      .finally(() => setLoading(false))
+    if (responseData.success) {
+      localStorage.setItem('auth-token', responseData.token);
+      toast.success('Login success', {
+        autoClose: 300,
+        onClose: () => navigate('/')
+      });
+      dispatch(login(responseData));
     } else {
-      toast.error(responseData.errors)
+      toast.error(responseData.errors);
     }
-    reset()
+    reset();
   };
 
   const onSubmit = async (data, event) => {
@@ -104,60 +119,67 @@ const LoginSignup = () => {
     } else {
       handleSignupSubmit(data);
     }
-  }
+  };
 
   return (
-    <div className="loginsignup">
-      <div className="loginsignup-container">
-        <h1>{state}</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="loginsignup-fields">
-            {state === "Signup" && (
-              <>
-                <input
-                  className={`form-control ${errors?.username?.message ? 'is-invalid' : ''}`}
-                  type="text"
-                  name="username"
-                  placeholder="Your name"
-                  {...register('username')}
-                />
-                <span style={{ color: 'red' }}>{errors?.username?.message}</span>
-              </>
-            )}
-            <input
-              className={`form-control ${errors?.email?.message ? 'is-invalid' : ''}`}
-              type="email"
-              name="email"
-              placeholder="Email Address"
-              {...register('email')}
-            />
-            <span style={{ color: 'red' }}>{errors?.email?.message}</span>
-            <input
-              className={`form-control ${errors?.password?.message ? 'is-invalid' : ''}`}
-              type="password"
-              name="password"
-              placeholder="Password"
-              {...register('password')}
-            />
-            <span style={{ color: 'red' }}>{errors?.password?.message}</span>
+    <>
+      {loading && (
+        <div className="spinner-overlay">
+          <Spinner />
+        </div>
+      )}
+      <div className="loginsignup">
+        <div className="loginsignup-container">
+          <h1>{state}</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="loginsignup-fields">
+              {state === "Signup" && (
+                <>
+                  <input
+                    className={`form-control ${errors?.username?.message ? 'is-invalid' : ''}`}
+                    type="text"
+                    name="username"
+                    placeholder="Your name"
+                    {...register('username')}
+                  />
+                  <span style={{ color: 'red' }}>{errors?.username?.message}</span>
+                </>
+              )}
+              <input
+                className={`form-control ${errors?.email?.message ? 'is-invalid' : ''}`}
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                {...register('email')}
+              />
+              <span style={{ color: 'red' }}>{errors?.email?.message}</span>
+              <input
+                className={`form-control ${errors?.password?.message ? 'is-invalid' : ''}`}
+                type="password"
+                name="password"
+                placeholder="Password"
+                {...register('password')}
+              />
+              <span style={{ color: 'red' }}>{errors?.password?.message}</span>
+            </div>
+            <button type="submit">Continue</button>
+          </form>
+          {state === 'Login' ? (
+            <p className="loginsignup-login">
+              Don't have an account? <span className="pointer cursor-pointer" onClick={setSignup}>Sign Up here</span>
+            </p>
+          ) : (
+            <p className="loginsignup-login">
+              Already have an account? <span className="pointer cursor-pointer" onClick={setLogin}>Login here</span>
+            </p>
+          )}
+          <div className="loginsignup-agree ">
+            <div><input className="text-center" type="checkbox" name='' id='' /></div>
+            <span>By continuing, I agree to the terms of use & privacy policy.</span>
           </div>
-          <button type="submit">Continue</button>
-        </form>
-        {state === 'Login' ? (
-          <p className="loginsignup-login">
-            Don't have an account? <span className="pointer cursor-pointer" onClick={setSignup}>Sign Up here</span>
-          </p>
-        ) : (
-          <p className="loginsignup-login">
-            Already have an account? <span className="pointer cursor-pointer" onClick={setLogin}>Login here</span>
-          </p>
-        )}
-        <div className="loginsignup-agree ">
-          <div><input className="text-center" type="checkbox" name='' id='' /></div>
-          <span>By continuing, I agree to the terms of use & privacy policy.</span>
         </div>
       </div>
-    </div >
+    </>
   );
 };
 
